69f17bf99ddbea844142c1f0dd5e57f7ed47f07c
Shahad Hassan <shahadg1983@gmail.com>
2024-03-27 22:20:08 +0530

    Adding copy functionality

--- src/components/Search/Card.jsx
+++ src/components/Search/Card.jsx
@@ -1,8 +1,12 @@
-
-
 export default function Card({color, value}){
+
+    const handleClick = () => {
+        window.navigator.clipboard.writeText(value)
+        alert('Color Code is copied to Clipboard.')
+    }
+
     return (
-        <div className="color-card z-0 h-40 w-72 rounded-md overflow-hidden relative group flex items-center justify-center border-2 border-white hover:border-black" style={{color: `${color}`}}>
+        <div onClick={handleClick} className="color-card cursor-pointer z-0 h-40 w-72 rounded-md overflow-hidden relative group flex items-center justify-center border-2 border-white hover:border-black" style={{color: `${color}`}}>
         <div className={`absolute h-28 w-28 -top-16 -right-16 rounded-full group-hover:scale-[625%] duration-500 z-10`} style={{backgroundColor: `${color}`}}></div>
             <div className="z-20 font-bold group-hover:text-[#810e0e] duration-500 text-2xl">
             <p>{color}</p>
