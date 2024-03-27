49e691d94a6f5b1602dd1148805aa3d4cd169a01
Shahad Hassan <shahadg1983@gmail.com>
2024-03-27 15:33:49 +0530

    Making responsive

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -9,15 +9,15 @@ export default function Search(){
     );
 
     return (
-        <div className="min-h-screen p-16 flex flex-col">
+        <div className="min-h-screen py-16 flex flex-col items-center md:items-start">
             <input
                 type="text"
                 placeholder="Search colors..."
                 value={searchTerm}
                 onChange={e => setSearchTerm(e.target.value)}
-                className="bg-[#3b3602] border-2 border-[#3e3e3e] rounded-lg text-white px-6 py-3 text-base hover:border-white cursor-pointer transition w-1/3 mb-10"
+                className="bg-[#3b3602] border-2 border-[#3e3e3e] rounded-lg text-white px-6 py-3 text-base hover:border-white cursor-pointer transition w-72 mb-10"
             />
-            <div className="grid grid-cols-3 gap-4 w-full">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center gap-4 w-full">
                 {filteredColors.map((color, index) => (
                     <Card key={index} color = {color.color} value = {color.value} />
                 ))}
