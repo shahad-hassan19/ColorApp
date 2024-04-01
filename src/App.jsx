8b5d4b387381e2b1f001ad68214bc12ea1e48d18
Shahad Hassan <shahadg1983@gmail.com>
2024-04-01 13:28:16 +0530

    Adding Insight Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from './components/Header/Header.jsx';
 import Footer from './components/Footer/Footer.jsx';
 import { Outlet } from 'react-router';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App() {
 
 
   return (
       <div>
+        <SpeedInsights/>
         <Header />
         <div className='mt-28 px-9 md:px-10 lg:px-16'>
           <Outlet />
